843537452cf9108193dd1add9a33ce63c290ca36
JULIEN PELAMATTI <f37280@dsp0864421.atlas.edf.fr>
2021-02-17 10:44:29 +0100

    Update validation.R

--- ValidationR.R
+++ ValidationR.R
@@ -1,7 +1,10 @@
 rm(list = ls())
 library(R.utils)
 library(sensitivity)
-# source('sensiHSIC.R')
+# source('/home/f37280/Documents/Openturns/CDC HSIC/Codes/sensitivity-master/R/sensiHSIC.R')
+# source('/home/f37280/Documents/Openturns/CDC HSIC/Codes/sensitivity-master/R/base.R')
+# source('/home/f37280/Documents/Openturns/CDC HSIC/Codes/sensitivity-master/R/weightTSA.R')
+
 #Import des donnees
 Data = read.csv("sample.csv", sep = ',')  # read csv file 
 namev = names(Data)
@@ -17,22 +20,15 @@ for(i in 1:dim(X)[2])
   kernelX = c(kernelX,'rbf')
 }
 
-# x <- sensiHSIC(model = NULL, X = X, kernelX = kernelX, kernelY = 'rbf', estimator.type = 'V-stat', test.method = "Permutation", B = 1000)
-# x <- sensiHSIC(model = NULL, X = X, target =  list(c = 5., upper = TRUE,type = "exp1side", param = 1.), kernelX = kernelX, kernelY = 'rbf', estimator.type = 'V-stat', test.method = "Asymptotic")
-x <- sensiHSIC(model = NULL, X = X, kernelX = kernelX, kernelY = 'rbf',target = list(c = 5., upper = TRUE,type = "exp1side", param = 0.5), estimator.type = 'V-stat', test.method = "Asymptotic")
+# x <- sensiHSIC(model = NULL, X = X, kernelX = kernelX, kernelY = 'rbf', estimator.type = 'U-stat', test.method = "Permutation", B = 10000)
+# x <- sensiHSIC(model = NULL, X = X, kernelX = kernelX, kernelY = 'rbf', estimator.type = 'U-stat', test.method = "Asymptotic")
+# x <- sensiHSIC(model = NULL, X = X, target =  list(c = 5., upper = TRUE,type = "exp1side", param = 0.5), kernelX = kernelX, kernelY = 'rbf', estimator.type = 'V-stat', test.method = "Asymptotic")
+# x <- sensiHSIC(model = NULL, X = X, target =  list(c = 5., upper = TRUE,type = "indicTh", param = 0.5), kernelX = kernelX, kernelY = 'categ', estimator.type = 'U-stat', test.method = "Permutation", B = 10000)
+# x <- sensiHSIC(model = NULL, X = X, target =  list(c = 5., upper = TRUE,type = "indicTh", param = 0.5), kernelX = kernelX, kernelY = 'categ', estimator.type = 'U-stat', test.method = "Asymptotic")
+x <- sensiHSIC(model = NULL, X = X, kernelX = kernelX, kernelY = 'rbf',cond = list(c = 5., upper = TRUE,type = "exp1side", param = 0.5), estimator.type = 'V-stat', test.method = "Permutation", B = 1000)
 
                
 R = tell(x,y)
 print(R$S[[1]])
 print(R$HSICXY[[1]])
 print(R$Pvalue[[1]])
-
-# paramX = x$paramX
-# paramY = x$paramY
-
-# asymp_test_HSIC(X,y,kernelX = "rbf", paramX =ramX, kernelY = "rbf", paramY = paramY)
-
-param = 0.5
-c = 5.
-Y = y
-wY <- exp( - (c-Y)*((c-Y)>0) / (param * sd(Y)/5) )
